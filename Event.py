846495ab3f361d2c667829ab9c21418a0ad037d2
Tim Ancona <tsancona@dmacc.edu>
2023-04-13 17:14:29 -0500

    implemented ability to check participant times and sort participants as well as find out which participants don't have times yet

--- Event.py
+++ Event.py
@@ -1,4 +1,5 @@
 from collections import deque
+from Participant import Participant
 
 
 class Event:
@@ -38,6 +39,33 @@ class Event:
         participant_to_remove = self.participants.popleft()
         self.finished_participants.append(participant_to_remove)
         return participant_to_remove
+
+    def all_times_entered(self):
+        for participant in self.participants:
+            if not participant.times:
+                return False
+        return True
+
+    def get_remaining_participants(self):
+        remaining_participant_names = ''
+        for participant in self.participants:
+            if not participant.times:
+                remaining_participant_names += f'{participant.name}\n'
+        return remaining_participant_names if remaining_participant_names else 'No participants remaining'
+
+    def calc_all_averages(self):
+        for participant in self.participants:
+            participant.calc_average()
+        while self.participants:
+            self.remove_participant()
+
+    def sort_participants(self):
+        n = len(self.finished_participants)
+
+        for i in range(n):
+            for j in range(0, n-i-1):
+                if self.finished_participants[j].average > self.finished_participants[j + 1].average:
+                    self.finished_participants[j], self.finished_participants[j + 1] = self.finished_participants[j + 1], self.finished_participants[j]
 
     def __str__(self):
         return f'Event Name: {self._name}\nParticipants: {[participant.name for participant in self.participants]}'
